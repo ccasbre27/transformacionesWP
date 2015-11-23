f42bf2c74be3c0f6e60990d50865a5585a71ff84
Carlos Castro Brenes <ccasbre27@hotmail.com>
2015-11-23 22:57:01 +0000

    init

---
+++ transformaciones/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Transformaciones.Resources;
+
+namespace Transformaciones
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void Ellipse_ManipulationDelta(object sender, System.Windows.Input.ManipulationDeltaEventArgs e)
+        {
+            transform.TranslateX += e.DeltaManipulation.Translation.X;
+            transform.TranslateY += e.DeltaManipulation.Translation.Y;
+
+        }
+
+
+    }
+}
